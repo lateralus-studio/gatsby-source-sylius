f3ada45241345a3a4da14081f025ccae89e1e47b
Spiros Martzoukos <s.d.martzoukos@gmail.com>
2020-04-04 15:14:56 +0300

    Skip category creation

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,13 +2,17 @@ const validateOptions = require("./validateOptions");
 const createSchemaCustomization = require("./createSchemaCustomization");
 const createResolvers = require("./createResolvers");
 const sourceNodes = require("./sourceNodes");
-const provideProductsField = require("./provideProductsField");
+// const provideProductsField = require("./provideProductsField");
 
 exports.onPreBootstrap = validateOptions;
 exports.createSchemaCustomization = createSchemaCustomization;
 exports.createResolvers = createResolvers;
 exports.sourceNodes = sourceNodes;
-exports.onCreateNode = ({ node, getNode, createNodeId, actions }) => {
-  const { createNodeField } = actions;
-  provideProductsField({ node, getNode, createNodeId, createNodeField });
-};
+
+// Skip creating categories for now as 
+// it's causing builds to fail. Will and 
+// should revisit once we add categories.
+// exports.onCreateNode = ({ node, getNode, createNodeId, actions }) => {
+//   const { createNodeField } = actions;
+//   provideProductsField({ node, getNode, createNodeId, createNodeField });
+// };
